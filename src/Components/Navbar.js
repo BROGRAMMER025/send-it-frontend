ffd0509a1ae366d28b2a46895322b6db9c6a307d
ian <ianokipla@gmail.com>
2024-03-05 16:46:28 +0300

    updated Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css'; // Import your custom CSS file
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a href="/" className="navbar-item">
+            SENDIT
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <a href="/create-account" className="navbar-item">
+              Create Account
+            </a>
+            <a href="/login" className="navbar-item">
+              Log In
+            </a>
+            <a href="/create-order" className="navbar-item">
+              Create Order
+            </a>
+            <a href="/change-destination" className="navbar-item">
+              Change Destination
+            </a>
+            <a href="/cancel-order" className="navbar-item">
+              Cancel Order
+            </a>
+            <a href="/order-details" className="navbar-item">
+              Order Details
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
